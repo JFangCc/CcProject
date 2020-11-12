ea35f5ac0a13fc69d0a8852da32c5ebc849fdd80
Able-Fangjing <2249116188@qq.com>
2020-11-12 11:17:47 +0800

    柯里化

--- TencentSwift学习笔记&PPT/TencentSwiftStudy.playground/Pages/函数式编程.xcplaygroundpage/Contents.swift
+++ TencentSwift学习笔记&PPT/TencentSwiftStudy.playground/Pages/函数式编程.xcplaygroundpage/Contents.swift
@@ -5,3 +5,15 @@ import Foundation
 var str = "Hello, playground"
 
 //: [Next](@next)
+//柯里化
+
+func add2(_ v1: Int, _ v2: Int, _ v3: Int) -> Int { v1 - v2 + v3 }
+
+prefix func ~<A, B, C, D>(_ fn: @escaping (A, B, C) -> D) -> (C) -> (B) -> (A) ->(D) {
+    { c in { b in { a in fn(a, b, c) } } }
+}
+print((~add2)(30)(20)(10))
+
+
+//函子（Functor）
+// Array、Optional 支持map运算的类型，称为函子
