9ec3608fbed624a99f291e3be60101b51a877295
Able-Fangjing <2249116188@qq.com>
2021-04-29 17:44:46 +0800

    111

---
+++ TencentSwift学习笔记&PPT/Swift_1期_day01_21/代码/HelloWorld.playground/Pages/Page01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import PlaygroundSupport
+
+let view = UIView()
+view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+view.backgroundColor = UIColor.red
+PlaygroundPage.current.liveView = view
+
+let imageView = UIImageView(image: UIImage(named: "logo"))
+PlaygroundPage.current.liveView = imageView
+
+let vc = UITableViewController()
+vc.view.backgroundColor = UIColor.lightGray
+PlaygroundPage.current.liveView = vc
---
+++ TencentSwift学习笔记&PPT/Swift_1期_day01_21/代码/HelloWorld.playground/Pages/Page02.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+let a = 10
+let b = 20
+var c = a + b
+c += 10
+c += 20
+print(c)
+
+
